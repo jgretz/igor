2a1ac2f28931eecd1e0ba23a472ff4ec3cda9439
Josh Gretz <jgretz@truefit.io>
2022-01-24 15:30:18 -0500

    update to specify new timeout option and handler

--- src/igor.controller.ts
+++ src/igor.controller.ts
@@ -3,19 +3,30 @@ import {RabbitMqService} from '@jgretz/igor-rabbit';
 import {SocketService} from '@jgretz/igor-socket';
 import {CRUD, COMMAND, CrudEventArgs, CommandEventArgs} from '@jgretz/igor-data-microservice';
 
+const TIMEOUT = 10000;
+
+const connectCrud = (socket: SocketService, rabbit: RabbitMqService) => {
+  socket.subscribe(CRUD, async (args: CrudEventArgs, callback: any) => {
+    rabbit.send(CRUD, args.source, args, {
+      timeout: TIMEOUT,
+      handler: callback,
+    });
+  });
+};
+
+const connectCommand = (socket: SocketService, rabbit: RabbitMqService) => {
+  socket.subscribe(COMMAND, async (args: CommandEventArgs, callback: any) => {
+    rabbit.send(COMMAND, args.target, args, {
+      timeout: TIMEOUT,
+      handler: callback,
+    });
+  });
+};
+
 @Controller()
 export class IgorController {
   constructor(private socket: SocketService, private rabbit: RabbitMqService) {
-    socket.subscribe(CRUD, async (args: CrudEventArgs, callback: any) => {
-      rabbit.send(CRUD, args.source, args, (message) => {
-        callback(message.payload);
-      });
-    });
-
-    socket.subscribe(COMMAND, async (args: CommandEventArgs, callback: any) => {
-      rabbit.send(COMMAND, args.target, args, (message) => {
-        callback(message.payload);
-      });
-    });
+    connectCrud(socket, rabbit);
+    connectCommand(socket, rabbit);
   }
 }
